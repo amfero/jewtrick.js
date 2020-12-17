9aebb3109f5b41293f3e0fac30b2fa3625f91834
amfero <65131540+amfero@users.noreply.github.com>
2020-12-17 16:55:49 +0300

    Update jewtrick.js

--- jewtrick.js
+++ jewtrick.js
@@ -2,19 +2,13 @@ var request = require("request");
 var cheerio = require("cheerio");
 var url = "https://jewtrick.ml/server/jewtrickstatus.html";
 
-
 setInterval(function() {
 
     request(url, function (error, response, body) {
         var $page = cheerio.load(body),
             text = $page("body").text();
 
-            if(text === "0") {
-                console.log("сервер не отвечает")
-            }
-            if(text === "1") {
-              console.log("Уже поздно пизда")
-          }
+            //
 
           if(text === "2") {
               console.log("ДАВААААААААААААЙ")
