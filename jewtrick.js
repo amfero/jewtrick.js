4d8682e401e73561a1505c556028a7022ba0439d
amfero <65131540+amfero@users.noreply.github.com>
2020-12-21 16:34:03 +0300

    Update jewtrick.js

--- jewtrick.js
+++ jewtrick.js
@@ -6,16 +6,15 @@ var url = "https://jewtrick.ml/server/jewtrickstatus.html";
 setInterval(function() {
 
     request(url, function (error, response, body) {
-        var text = body;
 
-            if(text === "0") {
+            if(body === "0") {
                 console.log("сервер не отвечает")
             }
-            if(text === "1") {
+            if(body === "1") {
               console.log("Уже поздно пизда")
           }
 
-          if(text === "2") {
+          if(body === "2") {
               console.log("ДАВААААААААААААЙ")
           }
   });
