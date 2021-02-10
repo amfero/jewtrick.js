1c5894eea42f86a213ddd31252d086feaa6e9bdf
amfero <65131540+amfero@users.noreply.github.com>
2021-02-10 17:53:41 +0300

    новый версия апдейт

--- jewtrick.js
+++ jewtrick.js
@@ -1,13 +1,16 @@
 var request = require("request");
-var url = "https://jewtrick.ml/jewtrickstatus.html";
-
 
 setInterval(function() {
-
+    var url = "https://jewtrick.ml/jewtrickstatus.html"
     request(url, function (error, response, body) {
-          if(body === "2") {
-              console.log("ДАВААААААААААААЙ")
-          }
-  });
-
-          }, 2 * 1000)
+        if(body == "0") {
+            console.log('2b2t не отвечает')
+        }
+        if(body == '1') {
+            console.log('jew trick невозможен из-за количества игроков')
+        }
+        if(body == '2') {
+            console.log('ЖЕВТРИК ТАЙМ ЖЕВТРИК ТАЙМ ЖЕВТРИК ТАЙМ ЖЕВТРИК ТАЙМ ЖЕВТРИК ТАЙМ ')
+        }
+    });
+}, 2 * 1000);
